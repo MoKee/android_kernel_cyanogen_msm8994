341f612be430833ca77d7f5d9275c683d8c639a3
bob_lu <Bob_Lu@compal.com>
2015-09-01 15:09:10 +0800

    leds: leds-ktd-flash: reset lvp state on resume
    - We completely power down when not in use, so make
      sure the lvp state/min current state is correct on
      resume.
    
    SAMBAR-446
    
    Change-Id: Ia60842613ec4e6e104ae32cbb51aec53a70b7f69
    (cherry picked from commit 4cc688147e2c2996c38c275816975a372b354151)

--- drivers/leds/leds-ktd-flash.c
+++ drivers/leds/leds-ktd-flash.c
@@ -827,6 +827,8 @@ static int ktd_dev_pm_resume(struct device *dev)
 			gpio_request_one(flash_led->flash_node[i].flash_ctrl->gpio, GPIOF_OUT_INIT_HIGH, flash_led->flash_node[i].flash_ctrl->label);
 			gpio_request_one(flash_led->flash_node[i].flash_strobe->gpio, GPIOF_OUT_INIT_LOW, flash_led->flash_node[i].flash_strobe->label);
 			gpio_request_one(flash_led->flash_node[i].flash_tx->gpio, GPIOF_OUT_INIT_LOW, flash_led->flash_node[i].flash_tx->label);
+			led_ktd_flash_senddata(&flash_led->flash_node[i], 0x00); //Disable LVP
+			led_ktd_flash_set_min_current(&flash_led->flash_node[i], 0x07);
 			flash_led->flash_node[i].suspend_state = 0;
 		}
 	}
